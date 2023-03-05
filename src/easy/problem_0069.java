e28f9097db4c16269232b7e9b770e91eb1f35b9f
Trilonka <aniskov.stepan@mail.ru>
2023-03-05 23:23:48 +0300

    mySqrt

---
+++ src/easy/problem_0069.java
@@ -0,0 +1,21 @@
+package easy;
+
+/**
+ Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+ You must not use any built-in exponent function or operator.
+
+ For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ */
+public class problem_0069 {
+
+    public int mySqrt(int x) {
+        int low = 0, high = 46340;
+        while (low < high) {
+            int mid = (low+high)/2 + 1;
+            if (mid*mid > x) high = mid-1;
+            else low = mid;
+        }
+        return low;
+    }
+}
